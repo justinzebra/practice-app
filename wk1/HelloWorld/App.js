95b95351136846b4863457b2b6316cac8f778565
justinzebra <39110422+justinzebra@users.noreply.github.com>
2020-03-12 15:39:19 +0800

    Add files via upload

---
+++ wk1/HelloWorld/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110619034 邱俊瑋</Text>
+      <Text>practice 1</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
